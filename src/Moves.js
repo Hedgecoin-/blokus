6fc398fbd349e13f00a214599d3c503f84f438d8
Yannick Robert <yjmrobert@gmail.com>
2017-11-30 14:40:24 -0500

    implemented Z5

--- src/Moves.js
+++ src/Moves.js
@@ -675,16 +675,20 @@ function PlayZ5(board, piece){
   // Rotation clockwise
   switch (piece.rotation) {
     case '0':
+    case '180':
+      PieceToTile(board, piece, -1, -1);
+      PieceToTile(board, piece, -1, 0);
       PieceToTile(board, piece, 0, 0);
+      PieceToTile(board, piece, 1, 0);
+      PieceToTile(board, piece, 1, 1);
       break;
     case '90':
-      PieceToTile(board, piece, 0, 0);
-      break;
-    case '180':
-      PieceToTile(board, piece, 0, 0);
-      break;
     case '270':
+      PieceToTile(board, piece, -1, 1);
+      PieceToTile(board, piece, 0, -1);
       PieceToTile(board, piece, 0, 0);
+      PieceToTile(board, piece, 0, 1);
+      PieceToTile(board, piece, 1, -1);
       break;
     default:
       break;
