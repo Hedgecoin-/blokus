d1dad3341a54f1528026524cefa71b93b33e79d1
Yannick Robert <yjmrobert@gmail.com>
2017-11-30 10:50:39 -0500

    added I4

--- src/Moves.js
+++ src/Moves.js
@@ -12,6 +12,10 @@ export function PlayPiece(board, piece){
       break;
     case 'V3':
       PlayV3(board, piece);
+      break;
+    case 'I4':
+      PlayI4(board, piece);
+      break;
     default:
       break;
   }
@@ -93,6 +97,37 @@ function PlayV3(board, piece){
       board[piece.y][piece.x] = piece;
       board[piece.y-1][piece.x] = piece;
       board[piece.y][piece.x+1] = piece;
+      break;
+    default:
+      break;
+  }
+}
+function PlayI4(board, piece){
+  // Rotation clockwise
+  switch (piece.rotation) {
+    case '0':
+      board[piece.y][piece.x-1] = piece;
+      board[piece.y][piece.x] = piece;
+      board[piece.y][piece.x+1] = piece;
+      board[piece.y][piece.x+2] = piece;
+      break;
+    case '90':
+      board[piece.y-1][piece.x] = piece;
+      board[piece.y][piece.x] = piece;
+      board[piece.y+1][piece.x] = piece;
+      board[piece.y+2][piece.x] = piece;
+      break;
+    case '180':
+      board[piece.y][piece.x-2] = piece;
+      board[piece.y][piece.x-1] = piece;
+      board[piece.y][piece.x] = piece;
+      board[piece.y][piece.x+1] = piece;
+      break;
+    case '270':
+      board[piece.y-2][piece.x] = piece;
+      board[piece.y-1][piece.x] = piece;
+      board[piece.y][piece.x] = piece;
+      board[piece.y+1][piece.x] = piece;
       break;
     default:
       break;
